7081647977ec2797a6915acd6319b27bce7c63d9
Felix Satyaputra <fsatyaputra@gmail.com>
2018-03-19 22:14:14 +1100

    Troubleshooting, temporarily disable sbt test

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ set -e
 
 openssl aes-256-cbc -K $encrypted_7dbe1e2dc648_key -iv $encrypted_7dbe1e2dc648_iv -in .travis/travis-gh.enc -out .travis/travis-gh -d
 eval "$(ssh-agent -s)"
+ls -l .travis/
 chmod go-rwx .travis/travis-gh
 chmod u-wx .travis/travish-gh
 ssh-add .travis/travis-gh
