15693384cc52366cb4785fba88e1a9bacecf4ee4
Felix Satyaputra <fsatyaputra@gmail.com>
2018-03-19 22:27:27 +1100

    Use before install

--- release.sh
+++ release.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-set -e
-
-openssl aes-256-cbc -K $encrypted_7dbe1e2dc648_key -iv $encrypted_7dbe1e2dc648_iv -in .travis/travis-gh.enc -out .travis/travis-gh -d
-eval "$(ssh-agent -s)"
-
+set -ex
 set -x
-
-ls -l .travis/
-chmod 0600 .travis/travis-gh
-ssh-add .travis/travis-gh
-rm .travis/travis-gh
 
 git tag -f release-test-deleteme
 git push -f origin release-test-deleteme
