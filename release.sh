83c41bef6d28e591e59ac3273ad16ebc328f646c
Felix Satyaputra <fsatyaputra@gmail.com>
2018-03-19 22:16:09 +1100

    Better chmod

--- release.sh
+++ release.sh
@@ -4,13 +4,13 @@ set -e
 
 openssl aes-256-cbc -K $encrypted_7dbe1e2dc648_key -iv $encrypted_7dbe1e2dc648_iv -in .travis/travis-gh.enc -out .travis/travis-gh -d
 eval "$(ssh-agent -s)"
-ls -l .travis/
-chmod go-rwx .travis/travis-gh
-chmod u-wx .travis/travish-gh
-ssh-add .travis/travis-gh
-rm .travis/travis-gh
 
 set -x
+
+ls -l .travis/
+chmod 0600 .travis/travis-gh
+ssh-add .travis/travis-gh
+rm .travis/travis-gh
 
 git tag -f release-test-deleteme
 git push -f origin release-test-deleteme
